eb8fc1cfe3a3308646e745e23bcb5216806dfadb
mansikc <mansichanu6603@gmail.com>
2025-05-05 15:55:49 +0530

    Create __manifest__.py

---
+++ modules/__manifest__.py
@@ -0,0 +1,11 @@
+# cors_handler/__manifest__.py
+{
+    'name': 'CORS Handler',
+    'version': '1.0',
+    'author': 'iKart Solutions',
+    'category': 'Tools',
+    'summary': 'Adds CORS headers to all HTTP responses',
+    'depends': [],
+    'installable': True,
+    'application': False,
+}

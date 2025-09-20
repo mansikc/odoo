edcbd8ac10c90bc12e7081525fd033245818cafe
Krunal <krunalc@yahoo.com>
2025-09-20 18:14:29 +0530

    commit

---
+++ payment_square_terminal/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Square Payment Terminal',
+    'version': '1.0.0',
+    'summary': 'Integration for Square Payment terminal for Odoo',
+    'category': 'Accounting/Payment',
+    'author': 'KrunalC',
+    'depends': ['point_of_sale'],
+    'data': [
+        'views/square_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'payment_square_terminal/static/src/js/square_paymnet.js',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}

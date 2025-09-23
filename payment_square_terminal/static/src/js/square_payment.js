bb03d1fc430f1c4f0e24aec268fc05c3ce528465
Krunal <krunalc@yahoo.com>
2025-09-23 15:57:20 +0530

    square terminal

--- payment_square_terminal/static/src/js/square_payment.js
+++ payment_square_terminal/static/src/js/square_payment.js
@@ -55,5 +55,4 @@ export class SquareTerminalInterface extends PaymentInterface {
 
 // Register provider with POS
 SquareTerminalInterface.supported_payment_methods = ["square"];
-PaymentInterface.register(SquareTerminalInterface);
-
+PaymentInterface.register("square", SquareTerminalInterface);

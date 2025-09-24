3e9ac45eae0f279b175251bf41aa6dc1511419e1
mansikc <mansichanu6603@gmail.com>
2025-09-24 10:28:44 +0530

    Update __manifest__.py

--- payment_square_terminal/__manifest__.py
+++ payment_square_terminal/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'POS Square Terminal',
     'version': '1.0',
     'category': 'Point of Sale',
-    'depends': ['point_of_sale'],
+    'depends': ['base', 'point_of_sale'],
     'data': [
         'views/pos_payment_method_views.xml',
     ],

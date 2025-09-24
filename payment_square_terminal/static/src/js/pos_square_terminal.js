fc08eecbbe652343eb102e866fdba5a23a62bbd1
mansikc <mansichanu6603@gmail.com>
2025-09-24 13:29:19 +0530

    Update pos_square_terminal.js

--- payment_square_terminal/static/src/js/pos_square_terminal.js
+++ payment_square_terminal/static/src/js/pos_square_terminal.js
@@ -1,41 +1,46 @@
 odoo.define('pos_square_terminal.payment', function(require) {
     'use strict';
     const PaymentInterface = require('point_of_sale.PaymentInterface');
-    const {Gui} = require('point_of_sale.Gui');
+    const { Gui } = require('point_of_sale.Gui');
 
     const SquareTerminalPayment = PaymentInterface.extend({
-        send_payment_request: async function(cid) {
+        send_payment_request: async function() {
             this._super.apply(this, arguments);
             const order = this.pos.get_order();
             const line = order.selected_paymentline;
-            // Prepare payment data to send to backend
-            const data = {
+
+            const payment_data = {
                 amount: line.amount,
                 currency: this.pos.currency.name,
-                payment_method_id: line.payment_method.id,
-                access_token: this.payment_method.square_access_token,
-                location_id: this.payment_method.square_location_id,
-                device_code: this.payment_method.square_device_code,
                 order_ref: order.uid,
             };
-            // Call backend, in production use secure controller endpoint!
+
             try {
-                let result = await this.rpc({
+                const result = await this.rpc({
                     model: 'pos.payment.method',
-                    method: 'square_process_payment',
-                    args: [[this.payment_method.id], data],
+                    method: 'square_create_checkout',
+                    args: [this.payment_method.id, payment_data],
                 });
-                if (result && result.status === 'SUCCESS') {
-                    line.set_payment_status('success');
+
+                if (result.status === "success") {
+                    line.set_payment_status('done');
+                    Gui.showPopup('InfoPopup', {
+                        title: 'Payment successful',
+                        body: 'Payment processed on Square Terminal.',
+                    });
                 } else {
                     line.set_payment_status('retry');
+                    Gui.showPopup('ErrorPopup', {
+                        title: 'Payment error',
+                        body: 'Payment failed: ' + result.error,
+                    });
                 }
             } catch (err) {
-                Gui.showPopup('ErrorPopup', {
-                    title: "Square Payment Error",
-                    body: err.message,
-                });
                 line.set_payment_status('retry');
+                Gui.showPopup('ErrorPopup', {
+                    title: 'Payment error',
+                    body: 'Error sending payment request: ' + err.message,
+                });
             }
         },
     });

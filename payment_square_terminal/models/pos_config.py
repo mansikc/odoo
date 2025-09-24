6a2e4090116de95f32535546e4de1b2a63ba9dc9
Krunal <krunalc@yahoo.com>
2025-09-24 13:43:26 +0530

    view fix

--- payment_square_terminal/models/pos_config.py
+++ payment_square_terminal/models/pos_config.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class PosConfig(models.Model):
     _inherit = "pos.config"
 
-iface_payment_terminal = fields.Selection(
-        selection_add=[('square', 'Square Terminal')],
-        string="Payment Terminal"
+iface_payment_terminal_square = fields.Boolean(
+        string="Enable Square Terminal",
+        default=False
     )
